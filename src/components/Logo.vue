cbe335424eb1003a5bbb9bd9af8b880ee85efc08
unknown <lssoy77@gmail.com>
2023-01-11 17:51:34 +0900

    Feat: header부분 구현

---
+++ src/components/Logo.vue
@@ -0,0 +1,19 @@
+<template>
+  <RouterLink to="/" class="logo"><span>OMDbAPI</span>.COM</RouterLink>
+</template>
+
+<style lang="scss" scoped>
+@import "~/scss/main";
+
+.logo {
+  font-family: "Oswald", sans-serif;
+  font-variant-position: 20px;
+  color: $black;
+  &:hover {
+    color: $black;
+  }
+  span {
+    color: $primary;
+  }
+}
+</style>
--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,3 +1,4 @@
 <template>
   <h1>Home~!</h1>
+  <div class="btn btn-primary">Home입니다.</div>
 </template>
